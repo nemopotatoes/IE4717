9e7d4e404159c144ed4e72f080d5a00955de8449
nemopotatoes <jesgjx@gmail.com>
2022-09-13 15:14:55 +0800

    Exercise 1 - display date & time

---
+++ Week06/Exercises/time.js
@@ -0,0 +1,10 @@
+// get the current time
+alert("Say... Hello world!\nI will tell you the time.");
+
+// getch raw current date time information
+var today = new Date();
+
+// display
+    document.write(
+        "Today is " + today
+    );
