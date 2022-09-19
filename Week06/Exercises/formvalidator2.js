ffa6788b053c19e26176a64d0d5d4eefdf228dca
nemopotatoes <jesgjx@gmail.com>
2022-09-20 04:09:33 +0800

    exercise 3: form validation

---
+++ Week06/Exercises/formvalidator2.js
@@ -0,0 +1,9 @@
+//   Note: This script does not work with IE8
+
+// Get the DOM addresses of the elements and register 
+//  the event handlers
+
+var customerNode = document.getElementById("custName");
+var phoneNode = document.getElementById("phone");
+customerNode.addEventListener("change", chkName, false);
+phoneNode.addEventListener("change", chkPhone, false);
