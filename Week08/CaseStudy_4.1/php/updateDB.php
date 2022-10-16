55aaa0fdcc1e7751d7e7da774f675fb03f897cd2
nemopotatoes <jesgjx@gmail.com>
2022-10-16 20:11:01 +0800

    casestudy 4.1

--- Week08/CaseStudy_4.1/php/updateDB.php
+++ Week08/CaseStudy_4.1/php/updateDB.php
@@ -7,30 +7,33 @@ if (!$conn) {
 }
 //Declare variables 
 $justjava=$_POST['endless-just-java'];
+$justjavacheck=$_POST['justjavacheck'];
 $singleCafe=$_POST['single-cafeaulait'];
 $doubleCafe=$_POST['double-cafeaulait'];
+$cafecheck=$_POST['cafecheck'];
 $singleCap=$_POST['single-cappuccino'];
 $doubleCap=$_POST['double-cappuccino'];
+$capcheck=$_POST['cappuccinocheck'];
 
 for($i = 1; $i <= 5; $i++) {
-	if ($i == 1) {
+	if ($i == 1 && $justjava && $justjavacheck) {
 		$sql = "UPDATE products SET productPrice = $justjava WHERE productID = $i";
 		mysqli_query($conn, $sql);
-	} elseif ($i == 2) {
+	} else if ($i == 2 && $singleCafe && $cafecheck) {
 		$sql = "UPDATE products SET productPrice = $singleCafe WHERE productID = $i";
 		mysqli_query($conn, $sql);
-	} elseif ($i == 3) {
+	} else if ($i == 3 && $doubleCafe && $cafecheck) {
 		$sql = "UPDATE products SET productPrice = $doubleCafe WHERE productID = $i";
 		mysqli_query($conn, $sql);
-	} elseif ($i == 4) {
+	} else if ($i == 4 && $singleCap && $capcheck) {
 		$sql = "UPDATE products SET productPrice = $singleCap WHERE productID = $i";
 		mysqli_query($conn, $sql);
-	} elseif ($i == 5) {
+	} else if ($i == 5 && $doubleCap && $capcheck) {
 		$sql = "UPDATE products SET productPrice = $doubleCap WHERE productID = $i";
 		mysqli_query($conn, $sql);
 	}
 }
 mysqli_close($conn);
-header("refresh:0;url=../../updateprice.php");
+header("refresh:0;url=../../CaseStudy_4/updateprice.php");
 
 ?>
--- Week08/CaseStudy_4.1/updateprice.php
+++ Week08/CaseStudy_4.1/updateprice.php
@@ -30,7 +30,7 @@ include "php/filltable.php";
 		</nav>
 	</div>
 	<div class="content">
-		<h1>Coffee at JavaJam</h1>
+		<h2>Update Coffee Prices</h2>
 		<div class="content">
 			<form action="php/updateDB.php" method="post">
 				<table class="menutable">
@@ -40,9 +40,9 @@ include "php/filltable.php";
 						</td>
 						<td><b>Just Java</b></td>
 						<td>Regular house blend, decaffeinated coffee, 
-							or flavor of the day. <br>
-							<b>Endless Cup $<span id="justjavaPrice"></span><input name="endless-just-java"></b>
-							<?php insert_table_row(1) ?>
+							or flavor of the day. <br><br>
+							<b>Endless Cup $<?php insert_table_row(1) ?></b><span id="justjavaPrice"></span><br> New Price:<input formnovalidate name="endless-just-java">
+							
 						</td>
 					</tr>
 					<tr>
@@ -51,11 +51,11 @@ include "php/filltable.php";
 						</td>
 						<td><b>Cafe au Lait</b></td>
 						<td>House blended coffee infused into a smooth, 
-							steamed milk. <br>
-							<b>Single $<span id="cafesinglePrice"></span><input name="single-cafeaulait"> 
-							<?php insert_table_row(2) ?>
-							Double $<span id="cafedoublePrice"></span><input name="double-cafeaulait"></b>
-							<?php insert_table_row(3) ?>
+							steamed milk. <br><br>
+							<b>Single $<?php insert_table_row(2) ?></b><span id="cafesinglePrice"></span><br> New Price:<input formnovalidate name="single-cafeaulait"> 
+							
+							<br><br><b>Double $<?php insert_table_row(3) ?><span id="cafedoublePrice"></b></span><br> New Price:<input formnovalidate name="double-cafeaulait"></b>
+							
 						</td>
 					</tr>
 					<tr>
@@ -64,11 +64,11 @@ include "php/filltable.php";
 						</td>
 						<td><b>Iced Cappuccino</b></td>
 						<td>Sweetened espresso blended with icy-cold milk 
-							and served in a chilled glass. <br>
-							<b>Single $<span id="cappuccinosinglePrice"></span><input name="single-cappuccino">
-							<?php insert_table_row(4) ?>
-								Double $<span id="cappuccinodoublePrice"></span><input name="double-cappuccino"></b>
-								<?php insert_table_row(5) ?>
+							and served in a chilled glass. <br><br>
+							<b>Single $<?php insert_table_row(4) ?></b><span id="cappuccinosinglePrice"></span><br> New Price:<input formnovalidate name="single-cappuccino">
+							
+								<br><br><b>Double $<?php insert_table_row(5) ?></b><span id="cappuccinodoublePrice"></span><br> New Price:<input formnovalidate name="double-cappuccino"></b>
+								
 						</td>
 					</tr>
 					<tr>
